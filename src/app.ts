e5e4bb28f4f4694e2c4d02d3a90a58f770314ba2
alexander <aleksandr.adamov@noveogroup.com>
2020-03-04 11:10:53 +0300

    [develop] Add ProjectItem class to render project items in lists

--- src/app.ts
+++ src/app.ts
@@ -167,6 +167,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 /**
+ * Project item class
+ */
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
+/**
  * Deal with lists
  */
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
@@ -208,9 +230,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     // Fix projects duplication bug clear all existing 'li' inside 'ul' and add all projects again
     listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      // const listItem = document.createElement('li');
+      // listItem.textContent = prjItem.title;
+      // listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
 }
